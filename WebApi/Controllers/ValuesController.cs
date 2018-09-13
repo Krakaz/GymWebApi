0c33ec91af3da43a59b992349ecd5fc4d572fea2
Денис Кузнецов <krakaz238@gmail.com>
2018-09-13 11:18:59 +0400

    feature: Добавляет сваггер в проект

--- WebApi/Controllers/ValuesController.cs
+++ WebApi/Controllers/ValuesController.cs
@@ -11,6 +11,9 @@ namespace gym_webapi_template.Controllers
     public class ValuesController : ControllerBase
     {
         // GET api/values
+        /// <summary>
+        /// Получает список значений
+        /// </summary>
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
@@ -18,6 +21,9 @@ namespace gym_webapi_template.Controllers
         }
 
         // GET api/values/5
+        /// <summary>
+        /// Получает значение по идентификатору
+        /// </summary>
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
@@ -25,18 +31,27 @@ namespace gym_webapi_template.Controllers
         }
 
         // POST api/values
+        /// <summary>
+        /// Сохраняет значение
+        /// </summary>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
         // PUT api/values/5
+        /// <summary>
+        /// Изменяет значение
+        /// </summary>
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
         // DELETE api/values/5
+        /// <summary>
+        /// Удаляет значение
+        /// </summary>
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
