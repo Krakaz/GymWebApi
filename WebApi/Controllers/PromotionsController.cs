93eb9e58f6ee60a72b8edbb31dfb95f85b830dbd
Денис Кузнецов <krakaz238@gmail.com>
2018-10-02 20:32:53 +0400

    feature Добавляет контроллер для акций

---
+++ WebApi/Controllers/PromotionsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+
+    /// <summary>
+    /// Контроллер для работы с акциями
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PromotionsController : ControllerBase
+    {
+        // GET api/promotions
+        /// <summary>
+        /// Получает списо акций
+        /// </summary>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ WebApi/Models/Promotion.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace WebApi.Models
+{
+    /// <summary>
+    /// Акция
+    /// </summary>
+    [DataContract]
+    public class Promotion
+    {
+        /// <summary>
+        /// Идентификатор акции
+        /// </summary>
+        [DataMember(Name = "id")]
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Заголовок
+        /// </summary>
+        [DataMember(Name = "header")]
+        [Required]
+        public string Header { get; set; }
+
+        /// <summary>
+        /// Описание
+        /// </summary>
+        [DataMember(Name = "description")]
+        [Required]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Ссылка на картинку
+        /// </summary>
+        [DataMember(Name = "imageUrl")]
+        [Required]
+        public string ImageUrl { get; set; }
+    }
+}
